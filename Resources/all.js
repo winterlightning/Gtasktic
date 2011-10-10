562e9a22408a7ae5056fc75be25da5dabd471586
winterlightning <raycornellwang@gmail.com>
2011-10-10 21:15:10 +0800

    Fixed for better build

--- Resources/all.js
+++ Resources/all.js
@@ -265,6 +265,7 @@
               current = Task.find($(this).data("id"));
               current.order = $(this).index();
               current.listid = ($(this).parent().parent())[0].id;
+              current.time = (new Date().getTime()).toString();
               return current.save();
             });
           },
@@ -1022,6 +1023,42 @@
       }
     });
   })(jQuery);
+  (function($) {
+    var db, db_path;
+    db_path = Titanium.Filesystem.getFile(Titanium.Filesystem.getApplicationDataDirectory(), "gtasktic.db");
+    db = Titanium.Database.openFile(db_path);
+    String.prototype.replaceAll = function(strReplace, strWith) {
+      var reg;
+      reg = new RegExp(strReplace, "ig");
+      return this.replace(reg, strWith);
+    };
+    db.execute("CREATE TABLE IF NOT EXISTS keyval ( key TEXT, value TEXT )");
+    return Spine.Model.Local = {
+      extended: function() {
+        this.sync(this.proxy(this.saveLocal));
+        return this.fetch(this.proxy(this.loadLocal));
+      },
+      saveLocal: function() {
+        var result;
+        result = JSON.stringify(this);
+        db.execute("DELETE from keyval where key ='" + this.name + "'");
+        Titanium.API.debug(result);
+        result = result.replaceAll("'", "''");
+        Titanium.API.log('INFO', result);
+        return db.execute("INSERT INTO keyval (key, value) VALUES ('" + this.name + "', '" + result + "')");
+      },
+      loadLocal: function() {
+        var result, resultSet;
+        resultSet = db.execute("SELECT value FROM keyval WHERE key = '" + this.name + "' LIMIT 1");
+        result = resultSet.field(0);
+        if (!result) {
+          return;
+        }
+        result = JSON.parse(result);
+        return this.refresh(result);
+      }
+    };
+  })(jQuery);
   Task = Spine.Model.setup("Task", ["name", "done", "time", "duedate", "note", "order", "synced", "listid"]);
   Task.extend(Spine.Model.Local);
   Task.extend({
@@ -1069,54 +1106,9 @@
   exports.List = List;
   exports.Version = Version;
   exports.Initialized = Initialized;
-  Spine.Model.Local = {
-    extended: function() {
-      this.sync(this.proxy(this.saveLocal));
-      return this.fetch(this.proxy(this.loadLocal));
-    },
-    saveLocal: function() {
-      var result;
-      result = JSON.stringify(this);
-      return localStorage[this.name] = result;
-    },
-    loadLocal: function() {
-      var result;
-      result = localStorage[this.name];
-      if (!result) {
-        return;
-      }
-      result = JSON.parse(result);
-      return this.refresh(result);
-    }
+  String.prototype.replaceAll = function(strReplace, strWith) {
+    var reg;
+    reg = new RegExp(strReplace, "ig");
+    return this.replace(reg, strWith);
   };
-  (function($) {
-    var db, db_path;
-    db_path = Titanium.Filesystem.getFile(Titanium.Filesystem.getApplicationDataDirectory(), "gtasktic.db");
-    db = Titanium.Database.openFile(db_path);
-    db.execute("CREATE TABLE IF NOT EXISTS keyval ( key TEXT, value TEXT )");
-    return Spine.Model.Local = {
-      extended: function() {
-        this.sync(this.proxy(this.saveLocal));
-        return this.fetch(this.proxy(this.loadLocal));
-      },
-      saveLocal: function() {
-        var result;
-        result = JSON.stringify(this);
-        db.execute("DELETE from keyval where key ='" + this.name + "'");
-        Titanium.API.debug(result);
-        result = result.replace("'", "''");
-        return db.execute("INSERT INTO keyval (key, value) VALUES ('" + this.name + "', '" + result + "')");
-      },
-      loadLocal: function() {
-        var result, resultSet;
-        resultSet = db.execute("SELECT value FROM keyval WHERE key = '" + this.name + "' LIMIT 1");
-        result = resultSet.field(0);
-        if (!result) {
-          return;
-        }
-        result = JSON.parse(result);
-        return this.refresh(result);
-      }
-    };
-  })(jQuery);
 }).call(this);
