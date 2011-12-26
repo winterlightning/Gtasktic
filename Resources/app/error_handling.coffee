2b2a3139877309444e5285b4e9f883e60c1ccc97
winterlightning <raycornellwang@gmail.com>
2011-12-26 17:34:42 -0500

    Working error submission

--- Resources/app/error_handling.coffee
+++ Resources/app/error_handling.coffee
@@ -1,4 +1,20 @@
-window.onerror = myErrorHandler = (errorMsg, url, lineNumber) ->
-  total_message = errorMsg + " " + url + " " + lineNumber
+#this file has a function that sends all js error to a global form that 
 
+window.myErrorHandler = (errorMsg, url, lineNumber) ->
+  total_message = errorMsg + " " + url + " " + lineNumber  
+  total_message = encodeURIComponent(total_message)
+
+  #setup xhr
+  xhr = new XMLHttpRequest()
+  xhr.open("POST", "https://docs.google.com/spreadsheet/formResponse")
+  xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded")
+  xhr.onreadystatechange = (status, response) ->
+  if xhr.readyState is 4
+    window.obj = $.parseJSON(window.xhr.response)
   
+  #setup submit data
+  submit_data = "formkey=dExjNVkwM1JkQm1oYy1BMGRKVjlUaVE6MQ&entry.0.single=#{ total_message }&hl=en_US"
+  console.log( submit_data )
+  xhr.send( submit_data )
+  
+window.onerror = window.myErrorHandler
--- Resources/app/error_handling.js
+++ Resources/app/error_handling.js
@@ -1,5 +1,18 @@
-window.onerror = function myErrorHandler(errorMsg, url, lineNumber) {
-  total_message = errorMsg + " " + url + " " + lineNumber;
-  
-  //submit_error_form( total_message )
-};
+(function() {
+  window.myErrorHandler = function(errorMsg, url, lineNumber) {
+    var submit_data, total_message, xhr;
+    total_message = errorMsg + " " + url + " " + lineNumber;
+    total_message = encodeURIComponent(total_message);
+    xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://docs.google.com/spreadsheet/formResponse");
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function(status, response) {};
+    if (xhr.readyState === 4) {
+      window.obj = $.parseJSON(window.xhr.response);
+    }
+    submit_data = "formkey=dExjNVkwM1JkQm1oYy1BMGRKVjlUaVE6MQ&entry.0.single=" + total_message + "&hl=en_US";
+    console.log(submit_data);
+    return xhr.send(submit_data);
+  };
+  window.onerror = window.myErrorHandler;
+}).call(this);
