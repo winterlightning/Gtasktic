abc02c2fe7634b5b38072f49cf42a108697c3f6a
winterlightning <raycornellwang@gmail.com>
2012-01-05 21:32:32 -0500

    Fixed stuff for build

--- Resources/all.js
+++ Resources/all.js
@@ -672,26 +672,40 @@
   })();
   exports = this;
   exports.Set = Set;
-  Spine.Model.Local = {
-    extended: function() {
-      this.sync(this.proxy(this.saveLocal));
-      return this.fetch(this.proxy(this.loadLocal));
-    },
-    saveLocal: function() {
-      var result;
-      result = JSON.stringify(this);
-      return localStorage[this.name] = result;
-    },
-    loadLocal: function() {
-      var result;
-      result = localStorage[this.name];
-      if (!result) {
-        return;
+  (function($) {
+    var db, db_path;
+    db_path = Titanium.Filesystem.getFile(Titanium.Filesystem.getApplicationDataDirectory(), "gtasktic.db");
+    db = Titanium.Database.openFile(db_path);
+    String.prototype.replaceAll = function(strReplace, strWith) {
+      var reg;
+      reg = new RegExp(strReplace, "ig");
+      return this.replace(reg, strWith);
+    };
+    db.execute("CREATE TABLE IF NOT EXISTS keyval ( key TEXT, value TEXT )");
+    return Spine.Model.Local = {
+      extended: function() {
+        this.sync(this.proxy(this.saveLocal));
+        return this.fetch(this.proxy(this.loadLocal));
+      },
+      saveLocal: function() {
+        var result;
+        result = JSON.stringify(this);
+        db.execute("DELETE from keyval where key ='" + this.name + "'");
+        result = result.replaceAll("'", "''");
+        return db.execute("INSERT INTO keyval (key, value) VALUES ('" + this.name + "', '" + result + "')");
+      },
+      loadLocal: function() {
+        var result, resultSet;
+        resultSet = db.execute("SELECT value FROM keyval WHERE key = '" + this.name + "' LIMIT 1");
+        result = resultSet.field(0);
+        if (!result) {
+          return;
+        }
+        result = JSON.parse(result);
+        return this.refresh(result);
       }
-      result = JSON.parse(result);
-      return this.refresh(result);
-    }
-  };
+    };
+  })(jQuery);
   Finished = Spine.Model.setup("Finished", ["name", "done", "time", "duedate", "note", "order", "synced", "listid", "time_finished"]);
   Finished.extend(Spine.Model.Local);
   Task = Spine.Model.setup("Task", ["name", "done", "time", "duedate", "note", "order", "synced", "listid", "updated"]);
